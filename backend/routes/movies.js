709cf7a0f63e64b8f75f7758e63e4874b2640055
Pornpipat Kingchongcharoensuk <faith.kingchongcharoensuk@gmail.com>
2024-12-18 19:18:43 +0700

    First Commit

---
+++ backend/routes/movies.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const router = express.Router()
+
+router.get('/',(req,res) => {
+    res.json({mssg:'Get all movies!!!'})
+})
+router.get('/:id',(req,res) => {
+    res.json({mssg:'Get one movie!!!'})
+})
+router.post('/:id',(req,res) => {
+    res.json({mssg:'Post one movie'})
+})
+router.delete('/:id',(req,res) => {
+    res.json({mssg:'Delete one movie'})
+})
+router.patch('/:id',(req,res) => {
+    res.json({mssg:'Update one movie'})
+})
+
+module.exports = router
